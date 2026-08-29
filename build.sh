9045095db3841e92ae34f7008ed301a843a5387e
Thomas Zimmerman <tpz5005@psu.edu>
2026-08-29 17:52:51 -0400

    Streamlined build process with requirements

--- build.sh
+++ build.sh
@@ -9,15 +9,11 @@ echo " Installing npm..."
 echo "------------------------------------------"
 npm ci
 npm install
-npm run build
 
 echo "------------------------------------------"
 echo "Setting up Python venv..."
 echo "------------------------------------------"
-python3 -m venv venv
-source venv/bin/activate
-pip install --upgrade pip
-pip install fastapi uvicorn
+pip install -r requirements.txt
 
 echo "------------------------------------------"
 echo "Installation Complete!"
