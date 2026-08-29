a78bca857ab4d05bb17c552e761ee56ae4165631
Thomas Zimmerman <tpz5005@psu.edu>
2026-08-29 16:50:05 -0400

    Added a build script for the project

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "------------------------------------------"
+echo "Building the project..."
+echo "------------------------------------------"
+
+echo "------------------------------------------"
+echo " Installing npm..."
+echo "------------------------------------------"
+npm ci
+npm install
+npm run build
+
+echo "------------------------------------------"
+echo "Setting up Python venv..."
+echo "------------------------------------------"
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install fastapi uvicorn
+
+echo "------------------------------------------"
+echo "Installation Complete!"
+echo "To run enter this command: npm run dev"
+echo "------------------------------------------"
