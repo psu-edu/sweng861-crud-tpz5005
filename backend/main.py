fa67e6f114562190ae84abcf9afe734b26b1ed02
Thomas Zimmerman <tpz5005@psu.edu>
2026-08-29 00:02:56 -0400

    added backend api calls for hello and health that return small jsons

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,25 @@
-#Entry point for SWENG CRUD project
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Enable CORS for the frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000",
+                   "http://127.0.0.1:3000"],
+)
+
+#################################################
+
+@app.get("/api/hello")
+def get_hello():
+    return {"message": "Hello World!"}
+
+#################################################
+
+@app.get("/health")
+def health_status():
+    return {"status": "ok"}
+
+#################################################
