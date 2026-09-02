54754e8ca33e0f76a847ad4dedbe2d0c9454e9c4
Thomas Zimmerman <tpz5005@psu.edu>
2026-09-01 23:58:04 -0400

    updated build script

--- build.sh
+++ build.sh
@@ -14,20 +14,15 @@ npm run build
 echo "------------------------------------------"
 echo "Setting up Python venv..."
 echo "------------------------------------------"
-python3 -m venv venv
-source venv/bin/activate
+python3 -m venv .venv
+source .venv/bin/activate
 pip install --upgrade pip
 pip install fastapi uvicorn
-
-echo "------------------------------------------"
-echo "Setting up backend..."
-echo "------------------------------------------"
-nohup uvicorn backend.main:app --reload --host 127.0.0.1 --port 8000 > uvicorn.log 2>&1 &
 
 echo "------------------------------------------"
 echo "Installation Complete!"
 echo "------------------------------------------"
 
 echo "------------------------------------------"
-echo "To run enter this command: npm run dev"
+echo "To run enter this command: npm run start-all"
 echo "------------------------------------------"
