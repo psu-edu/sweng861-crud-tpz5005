6f88fbe652bcfa40628f9901c0b0062a31cc2eb4
Thomas Zimmerman <tpz5005@psu.edu>
2026-09-04 13:10:27 -0400

    Added exception handler for log in attempts with a basic logger. Improved the /api/hello endpoint a bit

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from fastapi import Depends, FastAPI, Request, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from authlib.integrations.starlette_client import OAuth
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, JSONResponse
 import uvicorn
 import pprint
 from slowapi import Limiter, _rate_limit_exceeded_handler
@@ -18,8 +18,14 @@ init_db()  # Initialize the database
 # Create rate limiter
 limiter = Limiter(key_func=get_remote_address)
 
+# Create logger
+# TODO: maybe astract this out to another script
+logging.basicConfig(level=logging.INFO)
+logger=logging.getLogger(__name__)
+
 app = FastAPI()
 
+# set up a limiter for log in
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
@@ -53,20 +59,46 @@ oauth.register(
 
 # Authentication verification helper function
 #-----------------------------------------------#
-async def get_current_user(request: Request):
+async def require_auth(request: Request):
 #-----------------------------------------------#
+    # get the user info
     user = request.session.get("user")
+
+    # if its null/failure throw an error
     if not user:
-        raise HTTPException(status_code = 401,
-                            detail="Authentication required for this endpoint")
+        raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,
+                            detail="Authentication access token is required")
+
     return user
 
+
+# Exception handler for excessive log in attempts
+@app.exception_handler(RateLimitExceeded)
+#-----------------------------------------------#
+async def handle_login_rate_exception(request: Request, exc: RateLimitExceeded):
+#-----------------------------------------------#
+    # get some info from the client
+    client_ip = request.client.host
+
+    # log into
+    logger.warning(f"Excessive login attempts from: {client_ip}")
+
+    # pretty sure code 429 is the right one here
+    return JSONResponse(
+        status_code=429,
+        content={"detail": "Login rate exceeded. 5 log ins allowed per minute."}
+    )
+
+
 #################################################
+
 
 # Service Provider login
 # @info: This is the endpoint that re-directs the user to the external
 #        github login page
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 @app.get("/auth/login")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 @limiter.limit("5/minute") # Only 5 logins per minute
 async def login(request: Request):
     # This redirect origin MUST match the frontend origin, otherwise 
@@ -74,10 +106,11 @@ async def login(request: Request):
     redirect_uri = "http://localhost:8000/auth/callback" 
     return await oauth.github.authorize_redirect(request, redirect_uri)
 
-#################################################
 
 # Recieving end from the eternal github login page
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 @app.get("/auth/callback")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 async def auth_callback(request: Request):
     try:
         # Ask github to validate the login token and return an 
@@ -85,8 +118,8 @@ async def auth_callback(request: Request):
         auth_token = await oauth.github.authorize_access_token(request)
 
         # With the auth token, ask github for information about user
-        response = await oauth.github.get("user", token=auth_token)
-        profile_info = response.json()
+        response_type = await oauth.github.get("user", token=auth_token)
+        profile_info = response_type.json()
 
         # print("---------- PROFILE INFO ----------")
         # pprint.pprint(profile_info)
@@ -123,46 +156,57 @@ async def auth_callback(request: Request):
     except Exception as error:
         raise HTTPException(status_code=400, detail="ERROR: Authentication failed")
 
-#################################################
 
 # Get active user data
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 @app.get("/api/user")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 async def get_active_user(request: Request):
     user = request.session.get("user")
     if not user:
         return {"authenticated": False}
     return {"authenticated": True, "user": user}
 
-#################################################
 
 # Logout user
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 @app.get("/auth/logout")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 async def logout(request: Request):
     request.session.clear()
     return RedirectResponse(url="http://localhost:3000/")
 
-#################################################
 
+# Authentication required
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 @app.get("/api/hello")
-def get_hello(user: dict = Depends(get_current_user)):
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+def get_hello(request: Request, user: dict = Depends(require_auth)):
+ 
+    session_userInfo = request.session.get("user")
+    username = session_userInfo.get("username")
+    
     user_email = get_data_field(user_id=1, field_name="email")
 
-    return {"message": f"Hello, {user_email}!"}
+    return {"message": f"Hello, {username}@{user_email}!"}
 
-#################################################
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 @app.get("/health")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 def health_status():
     return {"status": "ok"}
 
-#################################################
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 @app.get("/api/database")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 def print_database():
     return print_database_info()
 
+
 #################################################
+
 
 # Launch the backend server apon startup of the application
 if __name__ == "__main__":
