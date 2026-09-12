b9c78bbbba7c5abc2f7e0c0871adcdc8bad10217
Thomas Zimmerman <tpz5005@psu.edu>
2026-09-11 22:19:07 -0400

    Implemented https

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   server: {
     port: 3000,
     strictPort: true,
-    proxy: {
-      // Forwards to fix non-fatal console errors
-      // TODO: Continue investigation of non-fatal console errors
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-
-      },
-      '/health': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-      },
-    }
+    https: true,
   },
 });
